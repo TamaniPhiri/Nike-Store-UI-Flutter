c87065d7f806a3f7c8b7222b86e9dbdd95dfc9c7
tamani phiri <tamanigabriel0@gmail.com>
2023-09-18 20:17:08 +0200

    changes

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -7,31 +7,34 @@ class IntroPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 232, 232, 232),
       body: Center(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(25.0),
-              child: Image.asset(
-                'lib/images/nike_logo.png',
-                height: 270,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Image.asset(
+                  'lib/images/nike_logo.png',
+                  height: 270,
+                ),
               ),
-            ),
-            const Text(
-              "Just did it",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.w900,
+              const Text(
+                "Just did it",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w900,
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Text(
-              "Brand new sneakers and custom kicks my gee",
-              style: TextStyle(fontSize: 16),
-              textAlign: TextAlign.center,
-            )
-          ],
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                "Brand new sneakers and custom kicks my gee",
+                style: TextStyle(fontSize: 16),
+                textAlign: TextAlign.center,
+              )
+            ],
+          ),
         ),
       ),
     );
