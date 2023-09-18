849151c8ebe3501f4397bfbddda5f7e848f727c6
tamani phiri <tamanigabriel0@gmail.com>
2023-09-18 21:31:45 +0200

    changes

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,6 +12,7 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home'),
+        backgroundColor: Colors.black87,
       ),
       body: const Center(
         child: Text('Home'),
