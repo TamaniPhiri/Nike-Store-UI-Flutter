06d787ebdfb868a3d7ab5ffa35a47812b197dc07
tamani phiri <tamanigabriel0@gmail.com>
2023-09-18 23:50:12 +0200

    changes

--- lib/components/bottom_nav_bar.dart
+++ lib/components/bottom_nav_bar.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
 class BottomNavBar extends StatelessWidget {
-  void Function(int)? onTabChange;
-  BottomNavBar({super.key, required this.onTabChange});
+  final Function(int)? onTabChange;
+  const BottomNavBar({super.key, required this.onTabChange});
   @override
   Widget build(BuildContext context) {
     return Container(
