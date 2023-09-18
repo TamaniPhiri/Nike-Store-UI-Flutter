2e5d627b7debd65df969836fafe1823685780b2a
tamani phiri <tamanigabriel0@gmail.com>
2023-09-18 21:44:04 +0200

    changes

---
+++ lib/components/bottom_nav_bar.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+class BottomNavBar extends StatelessWidget {
+  const BottomNavBar({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const GNav(tabs: [
+        GButton(
+          icon: Icons.home,
+          text: "Shop",
+        ),
+      ]),
+    );
+  }
+}
