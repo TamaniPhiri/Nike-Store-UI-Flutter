161d9e3dd31e927fc32237ad760dfda30f566d17
tamani phiri <tamanigabriel0@gmail.com>
2023-09-18 18:15:37 +0200

    changes

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,1 +1,11 @@
-import "package:flutter/material.dart"
+import "package:flutter/material.dart";
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.lime,
+    );
+  }
+}
