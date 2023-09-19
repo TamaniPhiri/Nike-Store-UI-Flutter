bcc4d9025e7fffd0da70f4de0958193c426ea017
tamani phiri <tamanigabriel0@gmail.com>
2023-09-19 22:57:25 +0200

    changes

---
+++ lib/components/shoe_tile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ShoeTile extends StatelessWidget {
+  const ShoeTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../components/shoe_tile.dart';
+
 class ShopPage extends StatefulWidget {
   const ShopPage({super.key});
 
@@ -49,7 +51,10 @@ class _ShopPageState extends State<ShopPage> {
         ),
         const SizedBox(
           height: 25.0,
-        )
+        ),
+        Expanded(child: ListView.builder(itemBuilder: (context, index) {
+          return const ShoeTile();
+        }))
       ],
     );
   }
