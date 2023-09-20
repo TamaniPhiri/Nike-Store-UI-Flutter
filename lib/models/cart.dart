61c86e839ed23befab7abffe8a70874ea50b9608
tamani phiri <tamanigabriel0@gmail.com>
2023-09-20 15:24:42 +0200

    changes

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -2,7 +2,13 @@ import 'package:demo/models/shoe.dart';
 
 class Cart {
   // list of shoes for sale
-  List<Shoe> shoeShop = [];
+  List<Shoe> shoeShop = [
+    Shoe(
+        name: "Polar reece",
+        price: "540",
+        imagePath: "lib/images/nike_logo.png",
+        description: "Out of this world")
+  ];
   // list of items in user's cart
   // get list of items for sale
   // get cart
