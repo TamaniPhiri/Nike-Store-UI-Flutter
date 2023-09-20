1c896d5704d0911745339e1c2f85f6457223e31c
tamani phiri <tamanigabriel0@gmail.com>
2023-09-20 15:23:01 +0200

    changes

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -1,6 +1,8 @@
+import 'package:demo/models/shoe.dart';
+
 class Cart {
   // list of shoes for sale
-
+  List<Shoe> shoeShop = [];
   // list of items in user's cart
   // get list of items for sale
   // get cart
