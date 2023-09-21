bb8038f5fe67296a6724dda5d6de9ebaf42e22f3
tamani phiri <tamanigabriel0@gmail.com>
2023-09-21 07:28:45 +0200

    changes

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -1,4 +1,6 @@
+import 'package:demo/models/cart.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../components/shoe_tile.dart';
 import '../models/shoe.dart';
@@ -13,68 +15,73 @@ class ShopPage extends StatefulWidget {
 class _ShopPageState extends State<ShopPage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        // search bar
-        Container(
-          padding: const EdgeInsets.all(12.0),
-          margin: const EdgeInsets.symmetric(horizontal: 25.0),
-          decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 239, 239, 239),
-              borderRadius: BorderRadius.circular(8.0)),
-          child: const Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [Text("Search"), Icon(Icons.search_rounded)],
-          ),
-        ),
-        // message
-        const Padding(
-          padding: EdgeInsets.symmetric(vertical: 25.0),
-          child: Text("Brands made by skilled professionals"),
-        ),
-        // hot picks
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 25.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Text(
-                "Hot picks",
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              Text(
-                "see all",
-                style: TextStyle(color: Color.fromARGB(255, 23, 122, 3)),
-              )
-            ],
-          ),
-        ),
-        const SizedBox(
-          height: 10.0,
-        ),
-        Expanded(
-            child: ListView.builder(
-                itemCount: 4,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) {
-                  // create a shoe
-                  Shoe shoe = Shoe(
-                      name: "Air max",
-                      price: "180",
-                      description: "A work of art",
-                      imagePath: "lib/images/nike_logo.png");
-                  return ShoeTile(
-                    shoe: shoe,
-                  );
-                })),
-        const Padding(
-          padding: EdgeInsets.only(top: 25.0, right: 25.0, left: 25.0),
-          child: Divider(
-            color: Colors.white,
-          ),
-        )
-      ],
-    );
+    return Consumer<Cart>(
+        builder: (context, value, child) => Column(
+              children: [
+                // search bar
+                Container(
+                  padding: const EdgeInsets.all(12.0),
+                  margin: const EdgeInsets.symmetric(horizontal: 25.0),
+                  decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 239, 239, 239),
+                      borderRadius: BorderRadius.circular(8.0)),
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [Text("Search"), Icon(Icons.search_rounded)],
+                  ),
+                ),
+                // message
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 25.0),
+                  child: Text("Brands made by skilled professionals"),
+                ),
+                // hot picks
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        "Hot picks",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        "see all",
+                        style:
+                            TextStyle(color: Color.fromARGB(255, 23, 122, 3)),
+                      )
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 10.0,
+                ),
+                // list of shoes for sale
+
+                Expanded(
+                    child: ListView.builder(
+                        itemCount: 4,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context, index) {
+                          // create a shoe
+                          Shoe shoe = Shoe(
+                              name: "Air max",
+                              price: "180",
+                              description: "A work of art",
+                              imagePath: "lib/images/nike_logo.png");
+                          return ShoeTile(
+                            shoe: shoe,
+                          );
+                        })),
+                const Padding(
+                  padding: EdgeInsets.only(top: 25.0, right: 25.0, left: 25.0),
+                  child: Divider(
+                    color: Colors.white,
+                  ),
+                )
+              ],
+            ));
   }
 }
